4d4b210c2cf868cfa3daedd4594557dcea67eebe
王包东 <wangbaodong@deepway.ai>
2023-07-10 16:53:37 +0800

    feat: ica analysis

--- lat_plot.py
+++ lat_plot.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import time
+import time,datetime
 import re
 import argparse
 import csv
@@ -20,7 +20,10 @@ import matplotlib.pyplot as plt
 # roll_angle,flc_heading_error_bias,wheel_angle,steer_ratio,mpc_steer_angle,heading_err_compensation,
 # steering_compensation,angular_velocity,acceleration_x,acceleration_y,lateral_error_raw,d_rate,
 # intergral_steer_angle,lat_override,
-def process(path):
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['axes.unicode_minus'] = False 
+
+def process(path, id):
     # 读取外部数据
     print("-" * 40)
     print("read file: ", path)
@@ -30,16 +33,40 @@ def process(path):
     data = pd.read_csv(path,encoding='gbk')
     # xdata = data.iloc[:,0]
     xdata = data.loc[:, 'timestamp_sec']
-    # ydata = data.iloc[:,7]
-    ydata = data.loc[:, 'current_curvature']
+    x_time = range(len(xdata))
+    curvature_data = data.loc[:, 'current_curvature']
+    heading_data = data.loc[:, 'current_heading_error']
+    lateral_data = data.loc[:, 'current_lateral_error']
+    print(len(lateral_data))
     plt.figure(1)
+    
+    plt.sca(plt.subplot(3, 1, 1))
+    plt.title(u'current_curvature', size=10)
     plt.legend()
-    # plt.scatter(xdata,ydata,s=1)
-    plt.plot(xdata, ydata, 'bo-', label=u'ee', linewidth=1)
-    plt.xlabel(u'time',size=10)
-    plt.ylabel(u'line',size=10)
+    plt.plot(x_time, curvature_data, color='b')
+    plt.ylabel('y',size=10)
+    plt.ylim(-0.0006,0.0006)
+    plt.grid()
+    
+    plt.sca(plt.subplot(3, 1, 2))
+    plt.title(u'current_heading_error', size=10)
+    plt.legend()
+    plt.plot(x_time, heading_data, color='b')
+    plt.ylabel('y',size=10)
+    plt.grid()
+    
+    plt.sca(plt.subplot(3, 1, 3))
+    plt.title(u'current_lateral_error', size=10)
+    plt.legend()
+    plt.plot(x_time, lateral_data, color='b')
+    plt.xlabel('x',size=10)
+    plt.ylabel('y',size=10)
+    plt.grid()
+    plt.savefig('../'+id+'.jpg')
     plt.show()     
             
 if __name__ == '__main__':
-    file = "/home/one/calib_data/1801/1801_5/ica/mlog/log-0/HAVP/lat_controller_log_2023-07-06_110555.csv"
-    process(file)
+    car_id = "b04"
+    # file = "/home/one/calib_data/1801/1801_5/ica/mlog/log-0/HAVP/lat_controller_log_2023-07-06_110555.csv"
+    file = sys.argv[1]
+    process(file, car_id)
